08582a7f230d151cd41d1d4374097a0478dbb776
hendidwipurwanto <hendi.dp@gmail.com>
2022-11-19 12:33:47 +0700

    setup dependency injection

---
+++ Capaci.Web/Capaci.BLL/TestService.cs
@@ -0,0 +1,22 @@
+﻿using Capaci.BLL.interfaces;
+using Capaci.DAL.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capaci.BLL
+{
+    public class TestService : ITestService
+    {
+        private readonly ITestRepository _testRepository;
+        public TestService(ITestRepository testRepository)
+        {
+            _testRepository = testRepository;
+        }
+
+        public string GetTest()
+        {
+           return _testRepository.GetTest();
+        }
+    }
+}
---
+++ Capaci.Web/Capaci.BLL/interfaces/ITestService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capaci.BLL.interfaces
+{
+    public interface ITestService
+    {
+        string GetTest();
+    }
+}
